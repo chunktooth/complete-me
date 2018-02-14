500c23353727cbd3ea31f469d910102591bc0c9a
chunktooth <om.saetehn@gmail.com>
2018-02-14 14:42:52 -0700

    Changed variables

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -12,6 +12,15 @@ describe('NODE', () => {
    expect(node).to.exist
  });
 
+ it('should take a letter', () => {
+   expect(node.letter).to.equal('w')
+ });
+
+ it('should have a defaulted letter value of null if no letter in inserted', () => {
+   let node = new Node();
+   expect(node.letter).to.equal(null)
+ })
+
  it('should have a default complete word value of false', () => {
    expect(node.completeWord).to.equal(false)
  });
